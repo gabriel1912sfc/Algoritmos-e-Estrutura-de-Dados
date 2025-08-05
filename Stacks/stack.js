52682bfd67d80e46381a65783d9e0697f10f06d2
Gabriel Santos <gfonseca882@gmail.com>
2025-08-05 18:32:48 -0300

    Desenvolvendo uma stack simples em JavaScript

---
+++ Stacks/stack.js
@@ -0,0 +1,38 @@
+//Implementação de uma stack em JavaScript
+class Stack{
+  constructor(arr){
+    this.arr = []
+  }
+
+  push(item){
+    this.arr.push(item);
+  }
+
+  pop(){
+    if(!this.arr.length){
+      console.log("The array is empty!")
+    }
+    return this.arr.pop()
+  }
+
+  peek(){
+    if(!this.arr.length){
+      console.log("The array is empty!")
+    }
+    return console.log(this.arr[this.arr.length - 1])
+  }
+
+  size(){
+    return console.log(this.arr.length)
+  }
+
+}
+
+let stack = new Stack()
+stack.push(3)
+stack.push(4)
+stack.peek()
+stack.size()
+stack.pop()
+stack.peek()
+stack.size()
